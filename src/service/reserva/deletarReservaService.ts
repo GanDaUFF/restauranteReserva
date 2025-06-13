d5e991d57bbee119ed284808cffdc1c0c51df97b
GandaUFF <qwert12121@hotmail.com>
2025-06-12 23:52:54 -0300

    fix: ajustes para retirada de arquivos nao utilizados

--- src/service/reserva/deletarReservaService.ts
+++
@@ -1,5 +0,0 @@
-import { deletarReservaPorId } from '../../repository/reservaRepository';
-
-export const deletarReservaService = async (id: number) => {
-  return await deletarReservaPorId(id);
-};
--- src/service/reserva/listarReservaService.ts
+++
@@ -1,6 +0,0 @@
-import { listarTodasReservas } from '../../repository/reservaRepository';
-
-
-export const listarReservasService = async () => {
-  return await listarTodasReservas();
-};
